d95cb2220a0ee70b5f38e16bb02b5511919526ee
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-07-08 15:18:46 +0800

    fix: fixing docs (5)

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -9,10 +9,6 @@ export default function docs(app: Express) {
     swaggerUi.setup(swaggerOutput, {
       customCssUrl:
         "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.css",
-      customJs: [
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-bundle.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-standalone-preset.js",
-      ],
     })
   );
 }
