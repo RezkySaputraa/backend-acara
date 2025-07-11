f20a0f346bc7d113e2d2a1837b6e3049b02272f3
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-07-11 08:31:10 +0800

    fix: docs js file prod

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -13,5 +13,16 @@ export default function docs(app: Express) {
   //   "utf-8"
   // );
 
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerOutput, {
+      customCssUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.css",
+      customJs: [
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-bundle.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-standalone-preset.js",
+      ],
+    })
+  );
 }
