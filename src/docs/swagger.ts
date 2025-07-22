624d515a9bcb3c1a2a1259bc3ee5de56111b8804
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-07-22 17:26:08 +0800

    feat: complete api crud banner and ticket

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -62,6 +62,19 @@ const doc = {
       RemoveMediaRequest: {
         fileUrl: "",
       },
+      CreateBannerRequest: {
+        title: "Banner 2 - Title",
+        image:
+          "https://res.cloudinary.com/dnc0k567c/image/upload/v1753173661/mmbr5dhpa3i3os8onruh.jpg",
+        isShow: false,
+      },
+      CreateTicketRequest: {
+        price: 1500,
+        name: "Ticket Reguler",
+        events: "687f28f3e1ec19c495892a1a",
+        description: "Ticket Reguler - description",
+        quantity: 100,
+      },
     },
   },
 };
